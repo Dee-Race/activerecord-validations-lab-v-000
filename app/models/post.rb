62f8e96399bb96eb60b4b53d6d7ea1f64a83a41d
Dee Race <deerace12@gmail.com>
2018-06-15 16:57:03 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,14 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validates :clickbaity 
+
+  private 
+
+  def clickbaity
+    if self.title
+      unless self.title.match(/(Won't Believe|Secret|Top \d*|Guess)/)
+        errors[:title] << "That's not clickbait-y"
+      end
+    end
+  end
 end
